4013b46faed35607268419a9e0671b8d03c376fc
ozdemirmelih <ozdemirmelih@yandex.com>
2020-02-03 10:54:03 +0300

    put Word Arrays to strings.xml

--- app/src/main/java/com/example/puretonetools/SrtFragment.java
+++ app/src/main/java/com/example/puretonetools/SrtFragment.java
@@ -95,8 +95,8 @@ public class SrtFragment extends Fragment implements OnClickListener {
         TextView srtText = (TextView) getView().findViewById(R.id.srtText);
         TextView pert = (TextView) getView().findViewById(R.id.yuzde);
         if (!isSDS) {
-            list = new String[]{"Hatıra", "Fotoğraf", "Hediye", "Tebeşir", "Sinema", "Tükenmez", "Salıncak", "Oduncu", "Harika", "Bayraklı", "Hamarat", "Hünerli", "Hemşire", "Havadar", "Paskalya", "Parmaklık", "Merdiven", "Sıradağ", "Asansör", "Coğrafya", "Harita", "Aracı ", "Pusula", "Telefon", "Şekerli", "Kafadar", "Yükseklik", "Kiracı", "Korkulu", "Satılık", "Kapalı", "Marmara", "Yasemin", "Kolonya", "Karanlık", "Badana", "Kaçamak", "Aydınlık", "Boyalı", "Yoğurtlu", "Hastalık", "Demirci", "Kulaklık", "Okyanus", "Dokuma", "Halıcı", "Sonbahar", "Kaymaklı", "İşitme", "Sarıyer", "Limonlu", "Adana", "Köstebek", "Cevizli", "İndirim", "Fıstıklı", "Tarafsız", "Kaplıca", "Çiçekli", "Akarsu", "Değerli", "Çilingir", "Yakacak", "Dönemeç", "Elbise", "Kızamık", "Papatya", "Giyecek", "Güvercin", "Kıvılcım", "Fabrika", "Aralık", "Sekreter", "Yasama", "Ağustos", "Ünite", "Çaydanlık", "Tüketim", "Otobüs", "Harabe", "Üretim", "Serinlik", "Postacı", "Dağıtım", "Tabure", "Tutacak", "Gemici", "Eflatun", "Danışma", "Arkadaş", "Değerli", "Çilingir", "Yakacak", "Dönemeç", "Elbise", "Kızamık", "Papatya", "Giyecek", "Güvercin", "Kıvılcım", "Fabrika", "Aralık", "Sekreter", "Yasama", "Ağustos", "Ünite", "Çaydanlık", "Tüketim", "Otobüs", "Harabe", "Üretim", "Serinlik", "Postacı", "Dağıtım", "Tabure", "Tutacak", "Gemici", "Eflatun", "Danışma", "Arkadaş", "Kızılcık", "Tabaka", "Kıymetli", "Cesaret", "Kahveci", "Lacivert", "Kanarya", "Çankırı", "Şikayet", "Karavan", "Begonya", "Öğrenci", "Hastane", "Lokanta", "Karanfil", "Pastane", "Domates", "Süpürge", "Köstebek", "Eczane", "Kanepe", "Haziran", "Kıvırcık", "Sürekli", "Sandalye", "Ihlamur", "Öneri", "Harita", "Denetim", "Kelime", "Kilimci", "Sipariş", "Patlıcan", "Gelincik", "Esinti", "Maydanoz", "Kitaplık", "Akasya", "Gezinti", "Gelenek", "Makina", "Hatalı", "Emanet", "Görenek", "Çekmece", "Bereket", "Tedavi", "Baharat", "Bankacı", "Kırmızı", "Hareket", "Eleman", "Etiket", "Almanya", "Hazine", "Derece", "Sigorta", "Hürriyet", "Hikaye", "Sanayi"
-            };
+            list = getResources().getStringArray(R.array.srt_list);
+
 
             switch (v.getId()) {
 
@@ -132,7 +132,8 @@ public class SrtFragment extends Fragment implements OnClickListener {
                     switch (listcount) {
                         case (0):
                             list = null;
-                            list = new String[]{"Kas", "At", "Ney", "Öç", "Bir", "Küf", "Saz", "Fon", "Pes", "Yün", "Bek", "Pay", "Sel", "Aç", "Dün", "Koz", "Ürk", "Zar", "Boy", "Baş", "Türk", "Yaş", "Ver", "Çak", "Şap", "Sonuç:"};
+                            list = getResources().getStringArray(R.array.sds_1);
+
                             srtText.setText(list[0]);
                             i = 0;
                             dogru = 0;
@@ -142,7 +143,8 @@ public class SrtFragment extends Fragment implements OnClickListener {
 
                         case (1):
                             list = null;
-                            list = new String[]{"Az", "Borç", "Düş", "Et", "Hür", "Kaz", "Çok", "Muş", "Ol", "Leş", "Pot", "Bal", "Tuş", "Şef", "Pek", "Çiz", "Fer", "Hat", "Ve", "Öp", "İç", "Bel", "Kurt", "Yem", "Zıt", "Sonuç:"};
+                            list = getResources().getStringArray(R.array.sds_2);
+
                             srtText.setText(list[0]);
                             i = 0;
                             dogru = 0;
@@ -151,7 +153,7 @@ public class SrtFragment extends Fragment implements OnClickListener {
 
                         case (2):
                             list = null;
-                            list = new String[]{"Beş", "Göz", "İn", "Kar", "Laf", "Diş", "Muz", "Ak", "Örf", "Çat", "Koç", "Fal", "Ney", "Şen", "Ruh", "Dağ", "Tel", "Kız", "Set", "Yıl", "Kök", "Pil", "Zam", "Yık", "Bey", "Sonuç:"};
+                            list = getResources().getStringArray(R.array.sds_3);
                             i = 0;
                             srtText.setText(list[0]);
                             dogru = 0;
@@ -160,7 +162,7 @@ public class SrtFragment extends Fragment implements OnClickListener {
 
                         case (3):
                             list = null;
-                            list = new String[]{"Çay", "Ot", "Fil", "Ön", "Kor", "Al", "Sarp", "Ez", "Dost", "Kul", "Kem", "Sık", "Buz", "Nal", "Sap", "Raf", "Tül", "Cep", "Terk", "Kan", "Şal", "Güz", "Küp", "Din", "Loş", "Sonuç:"};
+                            list = getResources().getStringArray(R.array.sds_4);
                             i = 0;
                             dogru = 0;
                             srtText.setText(list[0]);
@@ -169,7 +171,7 @@ public class SrtFragment extends Fragment implements OnClickListener {
 
                         case (4):
                             list = null;
-                            list = new String[]{"Lav", "Kep", "Dik", "Biç", "Öt", "Ser", "Böl", "Var", "İp", "Zarf", "Rey", "Mis", "Post", "Af", "Sat", "Yar", "Nem", "Git", "Çar", "Sis", "Han", "Püf", "Sonuç:"};
+                            list = getResources().getStringArray(R.array.sds_5);
                             i = 0;
                             srtText.setText(list[0]);
                             dogru = 0;
@@ -178,7 +180,7 @@ public class SrtFragment extends Fragment implements OnClickListener {
 
                         case (5):
                             list = null;
-                            list = new String[]{"Kir", "Çan", "Öl", "Sen", "Kalp", "An", "Hiç", "Şok", "Far", "Mes", "Kim", "Çit", "Harf", "Nar", "Söz", "Cop", "Fiş", "Bas", "Kor", "Tay", "Şu", "Ek", "İz", "Kaç", "Lif", "Sonuç:"};
+                            list = getResources().getStringArray(R.array.sds_6);
                             i = 0;
                             srtText.setText(list[0]);
                             dogru = 0;
